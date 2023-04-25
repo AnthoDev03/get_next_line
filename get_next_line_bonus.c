2657617447e0940a5f0b44c4eb4b9f964f596b71
Anthony Rodriguez <anthrodr@c2r8p4.42lehavre.fr>
2023-04-25 12:13:09 +0200

    francinette ok

---
+++ get_next_line_bonus.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anthrodr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/22 10:44:23 by anthrodr          #+#    #+#             */
+/*   Updated: 2023/04/25 10:34:26 by anthrodr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*read_file(int fd, char *buffer)
+{
+	char	*buff;
+	int		read_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	read_bytes = 1;
+	while (!ft_strchr(buffer, '\n') && read_bytes != 0)
+	{
+		read_bytes = read(fd, buff, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[read_bytes] = '\0';
+		buffer = ft_strjoin(buffer, buff);
+	}
+	free(buff);
+	return (buffer);
+}
+
+char	*ft_line(char *buffer)
+{
+	int		i;
+	char	*s;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	s = (char *)malloc(sizeof(char) * (i + 2));
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && (buffer[i] != '\n'))
+	{
+		s[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] == '\n')
+	{
+		s[i] = buffer[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
+
+char	*ft_next_buffer(char *buffer)
+{
+	int		i;
+	int		c;
+	char	*s;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	s = (char *)malloc(sizeof(char) * (ft_strlen(buffer) - i + 1));
+	if (!s)
+		return (NULL);
+	i++;
+	c = 0;
+	while (buffer[i])
+		s[c++] = buffer[i++];
+	s[c] = '\0';
+	free(buffer);
+	return (s);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer[257];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 256)
+		return (0);
+	buffer[fd] = read_file(fd, buffer[fd]);
+	if (!buffer[fd])
+		return (NULL);
+	line = ft_line(buffer[fd]);
+	buffer[fd] = ft_next_buffer(buffer[fd]);
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anthrodr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/22 10:44:33 by anthrodr          #+#    #+#             */
+/*   Updated: 2023/04/25 10:25:32 by anthrodr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *str);
+char	*ft_next_buffer(char *buffer);
+char	*ft_line(char *buffer);
+char	*read_file(int fd, char *buffer);
+#endif
